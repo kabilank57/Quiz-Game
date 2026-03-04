b8ea6e97bc81defad93abd82bf0ce01cf02cd8b5
KABILAN K <162274753+kabilank57@users.noreply.github.com>
2026-03-04 22:54:28 +0530

    quiz updated
    Updated answers to use words instead of numbers and added a loop to play again.

--- quiz.py
+++ quiz.py
@@ -7,22 +7,22 @@ questions = [
     {"question": "What is the largest planet in our solar system? =", "answer": "Jupiter"},
     {"question": "Which ocean is the largest? =", "answer": "Pacific"},
     {"question": "Who is known as the father of computers? =", "answer": "charles babbage"},
-    {"question": "How many continents are there on Earth? =", "answer": "7"},
+    {"question": "How many continents are there on Earth? =", "answer": "seven"},
     {"question": "What is the chemical symbol for water? =", "answer": "h2o"},
     {"question": "Which is the longest river in the world? =", "answer": "nile"},
     {"question": "Who painted the Mona Lisa? =", "answer": "leonardo da vinci"},
     {"question": "Which is the smallest country in the world? =", "answer": "vatican city"},
-    {"question": "How many legs does a spider have? =", "answer": "8"},
+    {"question": "How many legs does a spider have? =", "answer": "eight"},
     {"question": "What is the currency of Japan? =", "answer": "yen"},
-    {"question": "Which gas do plants absorb from the atmosphere? =", "answer": "carbon dioxide"},
-    {"question": "Who discovered gravity? =", "answer": "isaac Newton"}
+    {"question": "Which gas do plants absorb from the atmosphere? =", "answer": "co2"},
+    {"question": "Who discovered gravity? =", "answer": "newton"}
 
 ]
 
 def kabiquiz():
     score = 0
 
-    random.shuffle(questions) #each time change the questions order"
+    random.shuffle(questions) #each time change the questions order
 
     for i in questions:
         user_answer = input(i["question"]).strip()
@@ -34,17 +34,25 @@ def kabiquiz():
     
 
     percentage = (score/len(questions)*100)
+    print("\n PERCENTAGE", percentage)
+
     if(percentage >= 90):
         print("You are A+ grade")
     elif(percentage >= 80):
         print("You are A grade")
     elif(percentage >= 70):
         print("You are B+ grade")
-    elif(percentage >= 60):
+    elif(percentage >= 60):   
         print("You are B grade")
     elif(percentage >= 50):
         print("You are C grade")
     else:
         print("RE")
 
-kabiquiz()
+while True:
+    kabiquiz()
+    choice = input("\n Do you want to play again?(yes/no): ").strip().lower()
+    
+    if choice == "no":
+        print("Program ended.")
+        break
